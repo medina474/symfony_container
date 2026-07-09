a5f51434681bcc00da002e9bf20fa0409361920a
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-07-09 07:49:36 +0200

    test: DevController

---
+++ tests/HomeControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+final class HomeControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', '/demo');
+        self::assertResponseIsSuccessful();
+        //self::assertPageTitleContains('Jardins de Cocagne');
+        //self::assertSame('Ensemble, cultivons la solidarité', $crawler->filter('h1')->first()->text());
+    }
+}
