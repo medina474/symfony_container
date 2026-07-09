6efe9511f5f68e4bfac0b9d8855b6144998f33e0
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-07-09 19:48:32 +0200

    Entity: Tncc Type de nom en clair ; cette variable permet d'écrire le nom complet dans le cas d'un libellé avec article, et d'adapter la charnière si le nom est utilisé dans une expression comme « la commune de Marseille », « l'arrondissement du Mans »,

---
+++ src/Entity/Tncc.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TnccRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TnccRepository::class)]
+class Tncc
+{
+    #[ORM\Id]
+    #[ORM\Column(type: Types::BIGINT)]
+    protected ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $article = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $charniere = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->article;
+    }
+
+    public function setArticle(string $article): static
+    {
+        $this->article = $article;
+        return $this;
+    }
+
+    public function getCharniere(): ?string
+    {
+        return $this->charniere;
+    }
+
+    public function setCharniere(string $charniere): static
+    {
+        $this->charniere = $charniere;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->article;
+    }
+}
