a3a4bf95de2a2149fb4ecadbe0a8ff821675ca1f
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-07-13 21:53:14 +0200

    fixture: user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Country;
 use App\Entity\Tncc;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
+
     public function load(ObjectManager $manager): void
     {
+        // php bin/console doctrine:fixtures:load --env=test
+
+        $user = new User();
+        $user->setEmail('admin@app.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
         $tncc = new Tncc();
         $tncc->setId(1);
         $tncc->setArticle("la ");
