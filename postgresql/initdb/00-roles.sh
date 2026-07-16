c31d4f4e67cd7f9504853622eb74f5683b0cd4a8
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-07-16 13:32:01 +0200

    Nommer les fichiers secret avec role

--- postgresql/initdb/00-roles.sh
+++ postgresql/initdb/00-roles.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
 
-BACKUP_PASSWORD=$(cat /run/secrets/backup_password)
-GRAFANA_PASSWORD=$(cat /run/secrets/grafana_password)
+ROLE_BACKUP_PASSWORD=$(cat /run/secrets/role_backup_password)
+ROLE_GRAFANA_PASSWORD=$(cat /run/secrets/role_grafana_password)
 
 psql -v ON_ERROR_STOP=1 \
     --username "$POSTGRES_USER" \
     --dbname "$POSTGRES_DB" <<EOF
 -- Backup
-create role backup login password '$BACKUP_PASSWORD';
+create role backup login password '$ROLE_BACKUP_PASSWORD';
 grant connect on database $POSTGRES_DB to backup;
 grant pg_read_all_data to backup;
 grant pg_read_all_settings to backup;
 grant pg_read_all_stats to backup;
 
-create role grafana noinherit login password '$GRAFANA_PASSWORD';
+create role grafana noinherit login password '$ROLE_GRAFANA_PASSWORD';
 EOF
