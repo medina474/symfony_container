ea2ede9e9e3a4cb7adfd1dd285baa77bbf9d54e0
medina5 <emmanuel.medina@univ-lorraine.fr>
2026-07-16 13:25:57 +0200

    database: grafana password

--- postgresql/initdb/00-roles.sh
+++ postgresql/initdb/00-roles.sh
@@ -4,10 +4,12 @@ set -e
 BACKUP_PASSWORD=$(cat /run/secrets/backup_password)
 GRAFANA_PASSWORD=$(cat /run/secrets/grafana_password)
 
-psql -v ON_ERROR_STOP=1 <<EOF
+psql -v ON_ERROR_STOP=1 \
+    --username "$POSTGRES_USER" \
+    --dbname "$POSTGRES_DB" <<EOF
 -- Backup
 create role backup login password '$BACKUP_PASSWORD';
-grant connect on database $DATABASE_DB to backup;
+grant connect on database $POSTGRES_DB to backup;
 grant pg_read_all_data to backup;
 grant pg_read_all_settings to backup;
 grant pg_read_all_stats to backup;
